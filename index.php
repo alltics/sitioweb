f491739acde109a6a70149bdc74f27dfb26ecc33
AllticsMex <allticsmexico@gmail.com>
2024-03-23 10:57:33 -0600

    commidt inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hola Develoteca"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Sitio WEb</title>
+</head>
+<body>
+    Sitio Web
+</body>
+</html>
+
